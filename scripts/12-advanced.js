3b69a870e459e44d30efa6114524e2e86e961232
Harish <harishvigneshcoc@gmail.com>
2025-07-29 17:32:19 +0530

    modified

--- scripts/12-advanced.js
+++ scripts/12-advanced.js
@@ -27,3 +27,25 @@ document.querySelector('.js-button').addEventListener('click', function () {
         button.innerText = 'Finished!';
     }, 2000); // 1000 milliseconds = 1 second
 });
+
+const multiply = (x, y) => x * y
+console.log(multiply(7, 20 ));
+
+const countPositive = [1, -3, 5];
+let result = [];
+countPositive.forEach((num, index) => {
+    if(num > 0)
+        result.push(num);
+    return result;
+});
+console.log(countPositive);
+console.log(result);
+console.log(result.length);
+
+const arr = [1, 8, 3, 6, 8];
+let number = 2;
+const addNum = (array, num) => {
+    result = array.map(arr => arr + num);
+    return result;
+}
+console.log(addNum(arr, number));
